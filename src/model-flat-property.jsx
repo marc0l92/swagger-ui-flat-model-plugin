bd59ec1bce234528fa1c80f7441038b309604ea6
Marco LUCARELLA <marco.lucarella@amadeus.com>
2023-03-06 18:49:06 +0100

    Array of arrays

--- src/model-flat-property.jsx
+++ src/model-flat-property.jsx
@@ -16,43 +16,45 @@ export default class ModelFlatProperty extends Component {
         expandDepth: PropTypes.number
     }
 
-    getFilteredProperties(schema, extensions) {
+    getFilteredProperties(schema) {
+        const extensions = getExtensions(schema)
         return schema
             .filter((_, key) => ["example", "enum", "type", "description", "$$ref", "externalDocs", "items", "properties", "additionalProperties", "xml"].indexOf(key) === -1)
             .filterNot((_, key) => extensions.has(key))
     }
 
-    renderTypeAndProperties(schema) {
-        return <span className="prop-type">
-            {
-                type !== 'object' ? null : innerItem.name
-            }
-            {
-                type !== 'array' ? null : <>
-                    Array[{innerItem.name}{
-                        !(innerItem.properties && innerItem.properties.size) ? null : <span className="properties-list">({
-                            innerItem.properties.entrySeq().map(([key, v]) =>
-                                <span key={`${key}-${v}`} className={propClass} >{key}: {v}</span>
-                            )
-                        })</span>
-                    }]
-                    {/* <ModelFlatProperty
-                    getComponent={getComponent}
-                    getConfigs={getConfigs}
-                    schema={innerItem.schema}
-                    name={innerItem.name} /> */}
-                </>
-            }
-            {
-                !(type !== 'object' && type !== 'array') ? null : type
-            }
-        </span>
+    renderProperties(schema) {
+        const properties = this.getFilteredProperties(schema)
+        if (properties.size) {
+            return <span className="properties-list">({
+                properties.entrySeq().map(([key, v]) =>
+                    <span key={`${key}-${v}`} className={propClass} >{key}: {v}</span>
+                )
+            })</span>
+        }
+        return ''
+    }
+
+    renderTypeAndProperties(schema, namespace) {
+        const type = schema.get("type")
+        if (type === 'object') {
+            const name = getModelName(schema)
+            return <span className="prop-type">
+                <a href={'#' + namespace + '__' + name}>{name}</a>
+            </span>
+        } else if (type === 'array' && schema.get('items')) {
+            return <span className="prop-type">
+                Array[{this.renderTypeAndProperties(schema.get('items'), namespace)}{this.renderProperties(schema.get('items'))}]
+            </span>
+        } else {
+            return <span className="prop-type">
+                {type || 'object'}
+            </span>
+        }
     }
 
     render() {
         const { namespace, getComponent, getConfigs, schema, name } = this.props
-        console.log(name, schema.toJS())
-
         const { showExtensions } = getConfigs()
 
         if (!schema || !schema.get) {
@@ -60,13 +62,12 @@ export default class ModelFlatProperty extends Component {
             return <div></div>
         }
 
-        const type = schema.get("type")
         const xml = schema.get("xml")
         const enumArray = schema.get("enum")
         const description = schema.get("description")
         const example = schema.get("example")
         const extensions = getExtensions(schema)
-        const properties = this.getFilteredProperties(schema, extensions)
+        const properties = this.getFilteredProperties(schema)
         const externalDocsUrl = schema.getIn(["externalDocs", "url"])
         const externalDocsDescription = schema.getIn(["externalDocs", "description"])
 
@@ -74,104 +75,50 @@ export default class ModelFlatProperty extends Component {
         const Property = getComponent("Property")
         const Link = getComponent("Link")
 
-        let innerItem = null
-        let showProperties = false
-        if (type === 'object') {
-            innerItem = {
-                name: getModelName(schema),
-            }
-        } else if (type === 'array') {
-            if (schema.get('items')) {
-                const itemsType = schema.get('items').get('type')
-                if (itemsType === 'object') {
-                    innerItem = {
-                        name: getModelName(schema.get('items')),
-                        schema: schema.get('items'),
-                    }
-                } else {
-                    innerItem = {
-                        name: itemsType,
-                        schema: schema.get('items'),
-                        properties: this.getFilteredProperties(schema.get('items'), extensions),
-                    }
-                }
-            } else {
-                innerItem = { name: '' }
-            }
-            showProperties = true
-        } else {
-            showProperties = true
-        }
-
         return <span className="model">
             <span className="prop">
-                <span className="prop-type">
-                    {
-                        type !== 'object' ? null : <a href={'#' + namespace + '__' + innerItem.name}>{innerItem.name}</a>
-                    }
-                    {
-                        type !== 'array' ? null : <>
-                            Array[{innerItem.name}{
-                                !(innerItem.properties && innerItem.properties.size) ? null : <span className="properties-list">({
-                                    innerItem.properties.entrySeq().map(([key, v]) =>
-                                        <span key={`${key}-${v}`} className={propClass} >{key}: {v}</span>
-                                    )
-                                })</span>
-                            }]
-                            {/* <ModelFlatProperty
-                                getComponent={getComponent}
-                                getConfigs={getConfigs}
-                                schema={innerItem.schema}
-                                name={innerItem.name} /> */}
-                        </>
-                    }
-                    {
-                        !(type !== 'object' && type !== 'array') ? null : type
-                    }
-                </span>
                 {
-                    !showProperties ? null : <>
+                    this.renderTypeAndProperties(schema, namespace)
+                }
+                {
+                    !(properties && properties.size) ? null : <span className="properties-list">(
                         {
-                            !(properties && properties.size) ? null : <span className="properties-list">(
-                                {
-                                    properties.entrySeq().map(([key, v]) =>
-                                        <span key={`${key}-${v}`} className={propClass} >{key}: {v}</span>
-                                    )
-                                })</span>
-                        }
-                        {
-                            !(showExtensions && extensions.size) ? null : extensions.entrySeq().map(([key, v]) =>
-                                <Property key={`${key}-${v}`} propKey={key} propVal={v} propClass={propClass} />
+                            properties.entrySeq().map(([key, v]) =>
+                                <span key={`${key}-${v}`} className={propClass} >{key}: {v}</span>
                             )
-                        }
+                        })</span>
+                }
+                {
+                    !(showExtensions && extensions.size) ? null : extensions.entrySeq().map(([key, v]) =>
+                        <Property key={`${key}-${v}`} propKey={key} propVal={v} propClass={propClass} />
+                    )
+                }
+                {
+                    example &&
+                    <Property propKey="example" propVal={example} propClass={propClass} />
+                }
+                {
+                    enumArray && <span className="prop-enum">
+                        Enum: [ {enumArray.join(", ")} ]
+                    </span>
+                }
+                {
+                    description &&
+                    <Markdown source={description} />
+                }
+                {
+                    externalDocsUrl &&
+                    <div className="external-docs">
+                        <Link target="_blank" href={sanitizeUrl(externalDocsUrl)}>{externalDocsDescription || externalDocsUrl}</Link>
+                    </div>
+                }
+                {
+                    !(xml && xml.size) ? null : <div>
+                        <span className={propClass}>xml:</span>
                         {
-                            example &&
-                            <Property propKey="example" propVal={example} propClass={propClass} />
+                            xml.entrySeq().map(([key, v]) => <span key={`${key}-${v}`} className={propClass}><br />&nbsp;&nbsp;&nbsp;{key}: {String(v)}</span>).toArray()
                         }
-                        {
-                            enumArray && <span className="prop-enum">
-                                Enum: [ {enumArray.join(", ")} ]
-                            </span>
-                        }
-                        {
-                            description &&
-                            <Markdown source={description} />
-                        }
-                        {
-                            externalDocsUrl &&
-                            <div className="external-docs">
-                                <Link target="_blank" href={sanitizeUrl(externalDocsUrl)}>{externalDocsDescription || externalDocsUrl}</Link>
-                            </div>
-                        }
-                        {
-                            !(xml && xml.size) ? null : <div>
-                                <span className={propClass}>xml:</span>
-                                {
-                                    xml.entrySeq().map(([key, v]) => <span key={`${key}-${v}`} className={propClass}><br />&nbsp;&nbsp;&nbsp;{key}: {String(v)}</span>).toArray()
-                                }
-                            </div>
-                        }
-                    </>
+                    </div>
                 }
             </span>
         </span>
